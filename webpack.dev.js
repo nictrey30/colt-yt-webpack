3fc299eef0e01b5aa4fcb5bc1593dc7e075bcd49
Traian <niculescu.traian.ct@gmail.com>
2020-07-18 16:14:36 +0300

    added babel

--- webpack.dev.js
+++ webpack.dev.js
@@ -22,6 +22,16 @@ module.exports = merge(common, {
       {
         test: /\.s[ac]ss$/,
         use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
       }
     ]
   }
